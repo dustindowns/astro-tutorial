93f39bff9bd79042f996e9f494134d525049529b
Dustin Downs <ddowns@andmore.com>
2024-01-04 10:39:16 -0600

    add interactive greeting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://resonant-florentine-9ab193.netlify.app',
+  integrations: [preact()]
 });
